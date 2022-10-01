eaf3cf6b3818edb578cddb2ae9984b894016ae4a
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-10-01 20:56:46 +0530

    import java.util.*; class Add {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a,b,sum;         a = sc.nextInt();         b = sc.nextInt();         sum = a+b;         System.out.printf("%d",sum);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Add
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,sum;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sum = a+b;
+        System.out.printf("%d",sum);
+    }
+}
