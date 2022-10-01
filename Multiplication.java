88dc7a1387c93995e991244ae7878d537f2035d2
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-10-01 20:48:36 +0530

    import java.util.*; class Mul {     public static void main(String[] args)     {         int a,b,mul;         Scanner sc = new Scanner(System.in);         a = sc.nextInt();         b = sc.nextInt();         mul =a*b;         System.out.printf("%d",mul);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Mul
+{
+    public static void main(String[] args)
+    {
+        int a,b,mul;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        mul =a*b;
+        System.out.printf("%d",mul);
+    }
+}
