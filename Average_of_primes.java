d18600547744a73bb76c33d7db7e51fabc1bfe34
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-10-07 12:43:04 +0530

    import java.util.*; class lo{     static boolean prime(int n){         if(n<2)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++){             if(n%i==0)             return false;         }         return true;     }     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt();         int[] a=new int[n];         int c=0,d=0;         for(int i=0;i<n;i++){             a[i]=g.nextInt();             if(prime(a[i])){                 c+=a[i];                 d+=1;             }         }         System.out.printf("%.2f",c/(float)d);     } }

---
+++ Average_of_primes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class lo{
+    static boolean prime(int n){
+        if(n<2)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++){
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt();
+        int[] a=new int[n];
+        int c=0,d=0;
+        for(int i=0;i<n;i++){
+            a[i]=g.nextInt();
+            if(prime(a[i])){
+                c+=a[i];
+                d+=1;
+            }
+        }
+        System.out.printf("%.2f",c/(float)d);
+    }
+}
