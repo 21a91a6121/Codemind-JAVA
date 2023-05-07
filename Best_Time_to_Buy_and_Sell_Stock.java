5fd4c392c8d207e1f0cfd26bc072134b05a1d9d5
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2023-05-07 08:25:17 +0530

    import java.util.*; public class Profit {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int[] arr = new int[n];         for(int i=0;i<n;i++) {             arr[i] = sc.nextInt();         }         int max = 0;         for(int i=0;i<n;i++) {             for(int j=i+1;j<n;j++) {                 if(arr[i]<arr[j] && (arr[j]-arr[i])>max) {                     max = arr[j]-arr[i];                 }             }         }         System.out.print(max);     } }

---
+++ Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Profit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(arr[i]<arr[j] && (arr[j]-arr[i])>max) {
+                    max = arr[j]-arr[i];
+                }
+            }
+        }
+        System.out.print(max);
+    }
+}
