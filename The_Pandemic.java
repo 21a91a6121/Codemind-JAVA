058ddb3b183a9206121306054852db17c9b12289
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2023-05-11 15:20:01 +0530

    import java.util.*; public class Pandemic {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         if(a==2) {             System.out.println(1);         } else if(a==1){             System.out.println(0);         } else {             System.out.println(2);         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Pandemic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a==2) {
+            System.out.println(1);
+        } else if(a==1){
+            System.out.println(0);
+        } else {
+            System.out.println(2);
+        }
+    }
+}
